ba300d562618a204d94aaac77b3f51414c4876d8
iceiceninja <joshdbonham@gmail.com>
2023-03-01 19:59:35 -0600

    Added GUI and random start

--- src/gameoflife/GameOfLife.java
+++ src/gameoflife/GameOfLife.java
@@ -1,36 +1,47 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package gameoflife;
 
-import java.util.Arrays;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.util.Random;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
 
 /**
  *
  * @author Josh Bonham
  */
-public class GameOfLife {
+public class GameOfLife extends JPanel{
 
     /**
      * @param args the command line arguments
      */
+        
+    static Dimension screenDim = Toolkit.getDefaultToolkit().getScreenSize();
+    int generationCounter = 0;
+    static Cell [][] gameBoard;
+    private static final Random rnd = new Random();
+
     public static void main(String[] args) {
         // TODO code application logic here
-        Cell [][] gameBoard = createEmptyGameboard();
-        gameBoard[5][6].alive = true;
-        gameBoard[6][6].alive = true;
-        gameBoard[6][5].alive = true;
-        gameBoard[7][5].alive = true;
-        printBoard(gameBoard);
-        gameBoard = nextGeneration(gameBoard);
-        printBoard(gameBoard);
-        gameBoard = nextGeneration(gameBoard);
-        printBoard(gameBoard);
-        gameBoard = nextGeneration(gameBoard);
-        printBoard(gameBoard);
+        GameOfLife gol = new GameOfLife();
+        JFrame frame = new JFrame();
+        frame.getContentPane().add(gol);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(screenDim.width, screenDim.height);
+        frame.setVisible(true);
+        gameBoard = createEmptyGameboard();
+        gol.setupGrid();
+        
+        new Timer(100, (ActionEvent e) -> {
+            gol.nextGeneration();
+            gol.repaint();
+        }).start();
     }
-    static Cell[][] nextGeneration(Cell[][] gameBoard)
+    void nextGeneration()
     {
         Cell [][] oldGameboard = gameBoard;
         Cell[][] newGameboard = createEmptyGameboard();
@@ -69,36 +80,44 @@ public class GameOfLife {
                 }
             }
         }
-        return newGameboard;
+        gameBoard = newGameboard;
     }
     static Cell[][] createEmptyGameboard()
     {
-        Cell[][] gameBoard = new Cell[10][10];
-            for(int i =0; i < gameBoard.length;i++)
-            {
-                for(int j=0; j<gameBoard.length;j++)
-                {
-                    gameBoard[j][i] = new Cell();
-                }
+//        System.out.println(screenDim.width+""+screenDim.height);
+        Cell[][] gb = new Cell[screenDim.height][screenDim.width];//Cell[500][500];//Cell[screenDim.width][screenDim.height];
+        for (Cell[] row : gb) {
+            for (int j = 0; j < row.length; j++) {
+                row[j] = new Cell();
             }
-            return gameBoard;
-    }
-    static void printBoard(Cell[][] gameBoard)
-    {
-        for(int i = 0; i<gameBoard.length;i++)
-        {
-            for(int j = 0; j<gameBoard.length;j++)
-            {
-                if(gameBoard[j][i].alive == true)
-                {
-                    System.out.print("O");
-                }else
-                {
-                    System.out.print("X");
-                }
-            }
-            System.out.println();
         }
-        System.out.println("==================");
+        return gb;
+    }
+    private void setupGrid() {
+//        int counter=0;
+        for (Cell[] row : gameBoard) {
+            for (Cell cell : row) {
+                if (rnd.nextDouble() < 0.08) {
+                    cell.alive = true;
+                }
+            }
+        }
+    }
+        @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Color gColor = g.getColor();
+                
+        g.drawString("Generation: " + generationCounter++, 0, 10);
+        for (int i = 0; i < gameBoard.length; i++) {
+            for (int j = 0; j < gameBoard[i].length; j++) {
+                if (gameBoard[i][j].alive) {
+                    g.setColor(Color.CYAN);
+                    g.fillRect(j * 4, i * 4, 4, 4);
+                }
+            }
+        }
+
+        g.setColor(gColor);
     }
 }
